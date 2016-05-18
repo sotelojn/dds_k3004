91b698942b3fcc9b3749bdc1cd6136c373eeccfb
Juan <sotelo.jn@gmail.com>
2016-05-18 02:59:34 -0300

    Se agregaron más test de cercania

--- src/main/java/dominio/CGP.java
+++ src/main/java/dominio/CGP.java
@@ -3,34 +3,41 @@ package dominio;
 import java.time.LocalTime;
 import java.util.Set;
 
-public class CGP extends POI{
-	
+public class CGP extends POI {
+
 	private Comuna comuna;
 	private Servicio servicio;
 	private Set<Servicio> servicios;
 
-	
 	public CGP(String nombre, Double latitud, Double longitud) {
 		super(nombre, latitud, longitud);
 	}
-	
+
 	public CGP(String nombre, Posicion posicion) {
 		super(nombre, posicion);
 	}
-	
-	public boolean estaCerca(Posicion posicionActual){
+
+	public Comuna getComuna() {
+		return comuna;
+	}
+
+	public void setComuna(Comuna comuna) {
+		this.comuna = comuna;
+	}
+
+	public boolean estaCerca(Posicion posicionActual) {
 		return comuna.estaAdentro(posicionActual);
 	}
-	
-	public boolean estaDisponibleEnHorario(Servicio poi, LocalTime hora){
-		return  poi.getHorarioApertura().isAfter(hora) && poi.getHorarioCierre().isBefore(hora);
+
+	public boolean estaDisponibleEnHorario(Servicio poi, LocalTime hora) {
+		return poi.getHorarioApertura().isAfter(hora) && poi.getHorarioCierre().isBefore(hora);
 	}
-	
-	public boolean estaDisponible(Servicio servicio1, DiaDeAtencion dia, LocalTime hora){
-		return servicio1.getDias().contains(dia) && estaDisponibleEnHorario(servicio1,hora);
+
+	public boolean estaDisponible(Servicio servicio1, DiaDeAtencion dia, LocalTime hora) {
+		return servicio1.getDias().contains(dia) && estaDisponibleEnHorario(servicio1, hora);
 	}
-	
-	public boolean estaDisponible(DiaDeAtencion dia, LocalTime hora){
+
+	public boolean estaDisponible(DiaDeAtencion dia, LocalTime hora) {
 		return true;
 	}
 
--- src/test/java/dominio/TestCercania.java
+++ src/test/java/dominio/TestCercania.java
@@ -9,22 +9,28 @@ public class TestCercania {
 
 	Posicion posicionActual;
 	Posicion posicionLejana;
+	Posicion posicionComuna;
 	Banco bancoGalicia;
 	ParadaColectivo parada60;
 	Comercio kiosko;
+	CGP cgp;
+	Comuna comuna;
 	
 	@Before
 	public void setUp() {
 		posicionActual = new Posicion(1d, 1d);
 		posicionLejana = new Posicion(100d, 100d);
+		posicionComuna = new Posicion(1d, 1d);
 		bancoGalicia = new Banco("Galicia", 1d, 1d);
 		parada60 = new ParadaColectivo("Panamericana y Marquez", 1d, 1d);
-		
+		cgp = new CGP ("cgp",1d,1d);
 		kiosko = new Comercio("La petaca feliz", 1d, 1d);
 		Rubro golosinas = new Rubro();
 		golosinas.setDescripcion("Golosinas");
 		golosinas.setRangoCercania(10);
 		kiosko.setRubro(golosinas);
+		comuna = new Comuna(10d, posicionComuna);
+		cgp.setComuna(comuna);
 	}
 	
 	@Test
@@ -57,4 +63,14 @@ public class TestCercania {
 		assertFalse(parada60.estaCerca(posicionLejana));
 	}
 	
+	
+	@Test
+	public void testCgpEstaCercaDelMismoPunto() {
+		assertTrue(cgp.estaCerca(posicionActual));
+	}
+	
+	@Test
+	public void testCgpEstaLejosDeUnaPosicionLejana() {
+		assertFalse(cgp.estaCerca(posicionLejana));
+	}
 }
