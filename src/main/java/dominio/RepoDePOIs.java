3eb5cd374b0564e5d4eef86a871c94077ac060b5
RomanFerro <romanferro22@gmail.com>
2016-06-22 00:57:34 -0300

    AgregoBanco en el repoDePOIs

--- src/main/java/dominio/RepoDePOIs.java
+++ src/main/java/dominio/RepoDePOIs.java
@@ -1,5 +1,6 @@
 package main.java.dominio;
 
+import java.time.LocalTime;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -12,9 +13,26 @@ public class RepoDePOIs {
 	}
 	
 	
-	public static void agregarPOI (POI nombre) {
-		pois.add(nombre);
+	public static POI agregarBanco (String nombre, Posicion posicion, String calle, int altura, String descripcion, Set<String> tags, LocalTime horarioApertura, LocalTime horarioCierre, Set<Dia> diasDeAtencion) {
+		Banco nuevoBanco;
+		nuevoBanco = new Banco(nombre, posicion);
+		nuevoBanco.setCalle(calle);
+		nuevoBanco.setAltura(altura);
+		nuevoBanco.setDescripcion(descripcion);
+		nuevoBanco.setTags(tags);
+		nuevoBanco.setHorarioApertura(horarioApertura);
+		nuevoBanco.setHorarioCierre(horarioCierre);
+		nuevoBanco.setDiasDeAtencion(diasDeAtencion);
+		pois.add(nuevoBanco);
+		return nuevoBanco;
+		
 	}
+	
+//	public static POI agregarCGP (String nombre, Posicion posicion, String calle, int altura, String descripcion, Set<String> tags, LocalTime horarioApertura, LocalTime horarioCierre, Servicio servicio, Set<Servicio> servicios) {
+//		CGP nuevoCGP;
+//		nuevoCGP = new CGP(nombre, posicion);
+		
+//	}
 	
 	public static void borrarPOI (POI nombre) {
 		pois.remove(nombre);
