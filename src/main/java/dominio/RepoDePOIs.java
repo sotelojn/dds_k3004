152f76810fd691954d3125675b291bf6ee0c3e36
RomanFerro <romanferro22@gmail.com>
2016-06-22 00:03:09 -0300

    Cambio ConjuntoDePOIs por RepoDePOIs ya que es solicitado como un repositorio lo que creamos como un conjunto.

--- src/main/java/dominio/RepoDePOIs.java
+++ src/main/java/dominio/RepoDePOIs.java
@@ -1,13 +1,22 @@
-package dominio;
+package main.java.dominio;
 
 import java.util.Set;
 import java.util.stream.Stream;
 
-public class ConjuntoDePOIs {
+public class RepoDePOIs {
 	public static Set<POI> pois;
 	
 	public static Stream<POI> buscar (String text) {
 		return pois.stream()
 				   .filter(poi -> poi.matches(text));
+	}
+	
+	
+	public static void agregarPOI (POI nombre) {
+		pois.add(nombre);
+	}
+	
+	public static void borrarPOI (POI nombre) {
+		pois.remove(nombre);
 	}
 }
